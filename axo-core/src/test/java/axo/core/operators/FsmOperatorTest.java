8a48fe0f46d39673a91b5171806664ce41a492f2
Erik Orbons <e.m.orbons@gmail.com>
2015-09-28 08:39:07 +0200

    Tested state transitions in FSM operator.

--- axo-core/src/test/java/axo/core/operators/FsmOperatorTest.java
+++ axo-core/src/test/java/axo/core/operators/FsmOperatorTest.java
@@ -7,6 +7,8 @@ import org.testng.annotations.Test;
 import axo.core.StreamContext;
 import axo.core.producers.AbstractProducerTest;
 
+import static org.testng.Assert.*;
+
 public class FsmOperatorTest extends AbstractProducerTest<Long> {
 	@Override
 	public Publisher<Long> createFailedPublisher () {
@@ -38,12 +40,42 @@ public class FsmOperatorTest extends AbstractProducerTest<Long> {
 
 		@Override
 		public void handleInput (final Long input) {
-			produce (input);
+			pushState (state (this::handleInput0, this::handleComplete), input);
 		}
 
 		@Override
 		public void handleComplete() {
 			complete ();
+		}
+
+		public void handleInput0 (final Long input) {
+			assertTrue (input % 5 == 0);
+			produce (input);
+			gotoState (state (this::handleInput1, this::handleComplete));
+		}
+		
+		public void handleInput1 (final Long input) {
+			assertTrue (input % 5 == 1);
+			produce (input);
+			gotoState (state (this::handleInput2, this::handleComplete));
+		}
+		
+		public void handleInput2 (final Long input) {
+			assertTrue (input % 5 == 2);
+			produce (input);
+			gotoState (state (this::handleInput3, this::handleComplete));
+		}
+		
+		public void handleInput3 (final Long input) {
+			assertTrue (input % 5 == 3);
+			produce (input);
+			gotoState (state (this::handleInput4, this::handleComplete));
+		}
+		
+		public void handleInput4 (final Long input) {
+			assertTrue (input % 5 == 4);
+			produce (input);
+			popState ();
 		}
 	}
 }
